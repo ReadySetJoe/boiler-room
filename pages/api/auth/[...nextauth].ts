44e57a20ffbbfb90d1d9fa8e362e75e3e6420b3b
Joe Powers <54547147+ReadySetJoe@users.noreply.github.com>
2025-01-15 10:02:52 -0500

    attempt auth without importing prisma client

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,9 +4,6 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import TwitchProvider from 'next-auth/providers/twitch';
 import SteamProvider from 'next-auth-steam';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 const authOptions = (req: NextApiRequest): AuthOptions => ({
   adapter: PrismaAdapter(prisma),
